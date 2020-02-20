97f4074e7ddd8774fdb505274b8755aded96953b
Alex <akuzin20@gmail.com>
2020-02-20 23:16:04 +0300

    Initial commit

---
+++ src/test/java/com/example/todofullstackproject/TodoFullstackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todofullstackproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoFullstackProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
